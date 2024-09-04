b0c71ea04964ebc24df8f7b6563f412049be076c
Nahaq789 <naha022677@icloud.com>
2024-09-04 09:43:07 +0900

    while

--- main.cpp
+++ main.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main() {
-    // 入力
-    int N, k; cin >> N;
-    // 長さ N の可変長配列を宣言
-    vector<int> A(N);
-    // 配列の中身を受け取る
-    for(int i = 0; i < N; ++i) {
-        cin >> A[i];
-    }
+  int A, B;
+  cin >> A >> B;
 
-    // 左から k 番目の要素を出力
-    cout << A[0] << endl;
-    // 右から k 番目の要素を出力
-    cout << A[1] << endl;
-
-	return 0;
+  int i = 0;
+  int j = 0;
+  while (i < A) {
+    cout << "]";
+    i++;
+  }
+    cout << endl;
+  while (j < B) {
+    cout << "]";
+    j++;
+  }
 }
