e8ebfa387f6af6b236cc741a0a8e33540d8f9b94
Nahaq789 <naha022677@icloud.com>
2024-10-18 19:11:43 +0900

    STL

--- main.cpp
+++ main.cpp
@@ -3,53 +3,11 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
+    int A, B, C;
+    cin >> A >> B >> C;
 
-    string A;
-    vector<string> vec;
-    vector<string> chars;
+    int max1 = max(max(A, B), C);
+    int min1 = min(min(A, B), C);
 
-    int count = 0;
-
-    for (int i = 0; i < N; i++)
-    {
-        cin >> A;
-        vec.push_back(A);
-    }
-
-    sort(vec.begin(), vec.end());
-
-    for (int i = 0; i < N; i++)
-    {
-        if (vec[i] == "G" && vec[i + 1] != "G")
-        {
-
-            count++;
-            continue;
-        }
-        if (vec[i] == "W" && vec[i + 1] != "W")
-        {
-
-            count++;
-            continue;
-        }
-        if (vec[i] == "P" && vec[i + 1] != "P")
-        {
-
-            count++;
-            continue;
-        }
-        if (vec[i] == "Y" && vec[i + 1] != "Y")
-        {
-
-            count++;
-            continue;
-        }
-    }
-
-    if (count == 3)
-        cout << "Three";
-    if (count == 4)
-        cout << "Four";
+    cout << max1 - min1 << endl;
 }
