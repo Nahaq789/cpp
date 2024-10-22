3dad1fc446c6580c2eb20bf132437aba10929382
Nahaq789 <naha022677@icloud.com>
2024-10-22 11:30:34 +0900

    ABC081B - Shift only

--- main.cpp
+++ main.cpp
@@ -3,15 +3,38 @@
 #define rep(i, a, b) for (int i = a; i < b; i++)
 using namespace std;
 
-int N, A[101];
 int main()
 {
+    int N;
     cin >> N;
-    rep(i, 0, N) cin >> A[i];
-    sort(A, A + N, greater<int>());
+    vector<int> A;
 
-    int v[2] = {0, 0};
-    rep(i, 0, N) v[i % 2] += A[i];
-    int ans = v[0] - v[1];
-    cout << ans << endl;
+    int M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> M;
+        A.push_back(M);
+    }
+
+    int count = 0;
+    bool flg = true;
+    while (flg)
+    {
+        for (int &num : A)
+        {
+            if (num % 2 != 0)
+            {
+                flg = false;
+                break;
+            }
+            num /= 2;
+        }
+        if (!flg)
+        {
+            break;
+        }
+        count++;
+    }
+
+    cout << count << endl;
 }
